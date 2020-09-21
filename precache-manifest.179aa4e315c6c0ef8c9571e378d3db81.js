5f0d825b4c561a333c5105606a2d8a333b060a7f
Gianluca <gianlucazin1990@gmail.com>
2020-09-21 15:26:44 +0200

    Updates

---
+++ precache-manifest.179aa4e315c6c0ef8c9571e378d3db81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5654d5c765b0fd5682ec5a562ede8510",
+    "url": "/Markdown-previewer/index.html"
+  },
+  {
+    "revision": "a2381a0db825669e5838",
+    "url": "/Markdown-previewer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "80071b7b9b92d33cd0f7",
+    "url": "/Markdown-previewer/static/js/2.417e4531.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Markdown-previewer/static/js/2.417e4531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2381a0db825669e5838",
+    "url": "/Markdown-previewer/static/js/main.f7804cf9.chunk.js"
+  },
+  {
+    "revision": "f3f7ffbd8f59c5f28f34",
+    "url": "/Markdown-previewer/static/js/runtime-main.ca3572a5.js"
+  }
+]);
