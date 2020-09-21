fb46f3a1c78cdbea19d7412a324b616e924a0db4
Gianluca <gianlucazin1990@gmail.com>
2020-09-21 17:41:51 +0200

    state textarea set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Preview from './Preview.js';
+import Editor from  './Editor';
 
 function App() {
+  
   return (
     <div className="App">
-      <h1>Ciao</h1>
+      <Editor />
+      <Preview />
     </div>
   );
 }
