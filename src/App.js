69d651bd90611f52a58cfeaa76c403c226842c35
Gianluca <gianlucazin1990@gmail.com>
2020-09-21 23:27:05 +0200

    props functionality sorted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Preview from './Preview.js';
-import Editor from  './Editor';
+import Preview from './Components/Preview.js';
+import Editor from  './Components/Editor';
 
 function App() {
+ 
+  // Set the initial states with useState function
+  const [text, updText] = useState("Test");
+
+  // function to be exhecuted when something is typed
+  function handleText(event) {
+      // Udpate the states in here
+      updText(event.target.value);
+  }
   
   return (
     <div className="App">
-      <Editor />
-      <Preview />
+      <Editor value={text} setText={handleText} />
+      <Preview text={text} />
     </div>
   );
 }
